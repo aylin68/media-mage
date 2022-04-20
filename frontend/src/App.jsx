1cd22feffa42d836e93f5c84443271b451f0bdf0
aylin <aylin.mrLarki@gmail.com>
2022-04-20 10:51:38 +0200

    add login and registration and forgetpassword pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,25 @@
+import React from "react";
 import Home from "@pages/Home";
+import Login from "./components/Login";
+import Registration from "./components/Registration";
+import ForgetPassword from "./components/ForgetPassword";
+import Weather from "./components/Weather";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
 
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-      <p>coucou</p>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/registration" element={<Registration />} />
+          <Route path="/ForgetPassword" element={<ForgetPassword />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/pages/ForgetPassword.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../App.css";
+
+const ForgetPassword = () => {
+  return (
+    <>
+      <form className="form-login">
+        <div className="login">
+          <h3>Find Your Account</h3>
+          <div className="form-style">
+            <h6>
+              Please enter your email or mobile number to search for your
+              account.
+            </h6>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter your Email"
+            ></input>
+          </div>
+          <div className="button-style">
+            <Link to="/">
+              <button type="button" className="btn btn-secondary">
+                Cancel
+              </button>
+            </Link>
+            <button type="button" className="btn btn-primary">
+              Search
+            </button>
+          </div>
+        </div>
+      </form>
+    </>
+  );
+};
+export default ForgetPassword;
