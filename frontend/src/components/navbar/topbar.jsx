e791a575c5192a8ee717aa6ccb6ca370501c1cd3
Gilberto Mascarenhas <gilbertomascarenhas@Gilbertos-MacBook-Pro-2.local>
2022-04-26 17:10:56 +0200

    purple drip

--- frontend/src/components/navbar/topbar.jsx
+++ frontend/src/components/navbar/topbar.jsx
@@ -1,71 +1,71 @@
 import { React } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import './topbar.css'
+import "./topbar.css";
 import {
   Navbar,
-  Container,
   Nav,
   NavDropdown,
   Form,
   FormControl,
+  Container,
   Button,
+  ThemeProvider
 } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import logo from '../../assets/images/logo.svg'
 
 function Topbar() {
   return (
     <div className="topbar">
-      <Navbar bg="dark" variant="dark" expand="lg" className="ml-auto">
+      <Navbar variant="dark" expand="lg" className="ml-auto" sticky="top">
+          <img src={logo} alt="logo" className="logo" style={{display: 'flex', width: '3rem', height: 'auto', margin: '0, 1.5rem' }}/>
         <Container>
-          <Navbar.Brand>
-            <Link to="/">mediamage</Link>
+          <Navbar.Brand style={{display: 'flex !important', flexDirection: 'row !important', padding: '0 1rem !important  '}}>
+            <Link to="/" style={{ textDecoration: 'none', color: 'white' }}>mediamage</Link>
           </Navbar.Brand>
-              <Navbar.Collapse id="basic-navbar-nav">
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Navbar.Toggle aria-controls="responsivex-navbar-nav" />
             <Container>
-
-            <Form className="d-flex" >
-              <FormControl
-                type="search"
-                placeholder="Search"
-                className="me-2"
-                aria-label="Search"
-                
+              <Form className="d-flex">
+                <FormControl
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
                 />
-              <Button variant="outline-success">Search</Button>
-            </Form>
-                </Container>
-                <Container className="linkContainer">
-
-            <Nav className="me-auto">
-              <Nav.Link>
-                <Link to="/home">Home</Link>
-              </Nav.Link>
-              <Nav.Link>
-                <Link to="/profile/:user">Profile</Link>
-              </Nav.Link>
-              <NavDropdown
-                title="API's"
-                id="basic-nav-dropdown"
-                variant="dark"
-                bg="dark"
+                <Button variant="outline-success" color="blue">Search</Button>
+              </Form>
+            </Container>  
+            <Container expand="lg">
+              <Nav className="me-auto">
+                <Nav.Link>
+                  <Link to="/home" style={{ textDecoration: 'none', color: 'white' }}>Home</Link>
+                </Nav.Link>
+                <Nav.Link>
+                  <Link to="/profile/:user" style={{ textDecoration: 'none', color: 'white' }}>Profile</Link>
+                </Nav.Link>
+                <NavDropdown
+                  title="API's"
+                  id="basic-nav-dropdown"
+                  variant="dark"
+                  bg="dark"
                 >
-                <NavDropdown.Item>
-                  <Link to="/api/weather"></Link>
-                  Weather
-                </NavDropdown.Item>
-                <NavDropdown.Item>
-                  <Link to="/apis/vimeo">Vimeo</Link>
-                </NavDropdown.Item>
-                <NavDropdown.Item>Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item>Separated link</NavDropdown.Item>
-              </NavDropdown>
-              <Nav.Link>
-                <Link to="/logout">Log out</Link>
-              </Nav.Link>
-            </Nav>
-                </Container>
+                  <NavDropdown.Item>
+                    <Link to="/api/weather" style={{ textDecoration: 'none', color: 'white' }}></Link>
+                    Weather
+                  </NavDropdown.Item>
+                  <NavDropdown.Item>
+                    <Link to="/apis/vimeo" style={{ textDecoration: 'none', color: 'black' }}>Vimeo</Link>
+                  </NavDropdown.Item>
+                  <NavDropdown.Item>Something</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item>Separated link</NavDropdown.Item>
+                </NavDropdown>
+                <Nav.Link>
+                  <Link to="/logout" style={{ textDecoration: 'none', color: 'white' }}>Log out</Link>
+                </Nav.Link>
+              </Nav>
+            </Container>
           </Navbar.Collapse>
         </Container>
       </Navbar>
